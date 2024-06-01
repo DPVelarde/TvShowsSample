b566b6c6ea0a385b3c4fc1089948bba2e8caa3aa
Daniel Peña <danielpenavelarde@gmail.com>
2024-06-01 07:17:28 -0600

    - Add Hilt & Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -49,6 +51,10 @@ android {
     }
 }
 
+kapt {
+    correctErrorTypes = true
+}
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
@@ -59,6 +65,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-compiler:2.48.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 rootProject.name = "TvShowsSample"
 include(":app")
 include(":data")
+include(":framework")
